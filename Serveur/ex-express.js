791bffd0fa77cf04864ef53df7bb387403b15b7e
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-06-01 13:47:05 +0200

    Correction Express

--- Serveur/ex-express.js
+++ Serveur/ex-express.js
@@ -8,6 +8,8 @@ retourne rien, et définit un status code à 204
 
 2 / Répondre 404 si le contact 123 à déjà été supprimé
  */
+const express = require('express');
+
 const contacts = [{
     prenom: 'Jean',
     id: 123
@@ -18,3 +20,25 @@ const contacts = [{
     prenom: 'Martin',
     id: 789
 }];
+
+const app = express();
+
+app.get('/api/contact', (req, res, next) => {
+    res.json(contacts);
+});
+
+app.get('/api/contact/123', (req, res, next) => {
+    const contact = contacts.find(c => c.id === 123);
+    res.json(contact);
+});
+
+app.delete('/api/contact/123', (req, res, next) => {
+    const i = contacts.findIndex(c => c.id === 123);
+    contacts.splice(i, 1);
+    res.statusCode = 204;
+    res.end();
+});
+
+app.listen(8080, () => {
+   console.log('Server started');
+});
